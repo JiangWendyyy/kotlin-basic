e076d7ff7188b6702e3f75ab1ab73e16d732ee14
JiangWendyyy <j16698507162@163.com>
2024-03-19 16:51:12 +0800

    feat:make 26,3 to Z,AA,AB

--- src/main/kotlin/com/thoughtworks/kotlin_basic/util/NumberToRowUtil.kt
+++ src/main/kotlin/com/thoughtworks/kotlin_basic/util/NumberToRowUtil.kt
@@ -2,13 +2,23 @@ package com.thoughtworks.kotlin_basic.util
 
 class NumberToRowUtil {
     fun numberToRow(a: Int, b: Int): Array<String?> {
-        val result = Array<String?>(b) {null}
-        var row = a
-        for (i in 0..<b){
-            val number = ('A'.code + row - 1).toChar()
-            result[i] = number.toString()
-            row++
+        val resultArray = Array<String?>(b) {null}
+        for(i in a..<a + b){
+            var theLastCharInt = i
+            var theMiddleCharInt:Int? = null
+            if(i>26){
+                theLastCharInt = i.div(26)
+                theMiddleCharInt = a/26
+            }
+            var theLastChar: Char = intToChar(theLastCharInt)
+            val theMiddleChar: Char? = theMiddleCharInt?.let { intToChar(it) }
+            var resultString = theMiddleChar?.let { it.toString() + theLastChar.toString()} ?: theLastChar.toString()
+            resultArray[i-a] = resultString
         }
-        return result
+        return resultArray
+    }
+
+    private fun intToChar(row: Int): Char {
+        return ('A'.code + row - 1).toChar()
     }
 }
--- src/test/kotlin/com/thoughtworks/kotlin_basic/util/NumberToRowUtilTest.kt
+++ src/test/kotlin/com/thoughtworks/kotlin_basic/util/NumberToRowUtilTest.kt
@@ -5,9 +5,28 @@ import org.junit.jupiter.api.Test
 
 class NumberToRowUtilTest {
     @Test
+    fun `1,1 to A`() {
+        val numberToRowUtil = NumberToRowUtil()
+        val result = arrayOf("A")
+        println(java.util.Arrays.toString(numberToRowUtil.numberToRow(1, 1)))
+        assertTrue(result.contentEquals(numberToRowUtil.numberToRow(1, 1)))
+    }
+
+    @Test
     fun `1,2 to A,B`() {
         val numberToRowUtil = NumberToRowUtil()
         val result = arrayOf("A","B")
+        println(java.util.Arrays.toString(numberToRowUtil.numberToRow(1, 1)))
         assertTrue(result.contentEquals(numberToRowUtil.numberToRow(1, 2)))
     }
+
+    @Test
+    fun `26,3 to Z,AA,AB`() {
+        val numberToRowUtil = NumberToRowUtil()
+        val result = arrayOf("Z","AA")
+        println(java.util.Arrays.toString(numberToRowUtil.numberToRow(26, 2)))
+        assertTrue(result.contentEquals(numberToRowUtil.numberToRow(26, 2)))
+    }
+
+
 }
